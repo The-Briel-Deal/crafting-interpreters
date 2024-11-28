bf5e3aa4ff064fa50b6113f90e171ee1d9c10014
Gabriel Ford <gabe@fordltc.net>
2024-11-28 10:04:30 -0500

    Building linked list for challenge 3

--- chapter_1/challenge_3.c
+++ chapter_1/challenge_3.c
@@ -1,3 +1,36 @@
-
+#include <assert.h>
+#include <stddef.h>
 #include <stdio.h>
-int main(int argc, char **argv) { printf("Hello World"); }
+#include <stdlib.h>
+
+struct Node {
+  struct Node *next;
+  int index;
+};
+
+struct Node *init_linked_list() {
+  struct Node *head = malloc(sizeof(struct Node));
+  head->index = 0;
+  head->next = NULL;
+  return head;
+}
+
+void push_node(struct Node *head) {
+  struct Node *next = malloc(sizeof(struct Node));
+  next->index = -1;
+  next->next = NULL;
+  while (head->next != NULL) {
+    head = head->next;
+  }
+  assert(head->next == NULL);
+  head->next = next;
+}
+
+int main(int argc, char **argv) {
+  struct Node head = {.next = NULL, .index = 1};
+  struct Node next_node = {.next = NULL, .index = 2};
+  head.next = &next_node;
+
+  printf("head index is %i\n", head.index);
+  printf("head.next index is %i\n", head.next->index);
+}
