47aaec8236f93f55805bd8354f6df534ef9bc185
Gabriel Ford <gabe@fordltc.net>
2024-11-28 10:16:16 -0500

    Added an arg to set index.

--- chapter_1/challenge_3.c
+++ chapter_1/challenge_3.c
@@ -8,32 +8,30 @@ struct Node {
   int index;
 };
 
-struct Node *init_linked_list() {
+struct Node *init_linked_list(int index) {
   struct Node *head = malloc(sizeof(struct Node));
-  head->index = 0;
+  head->index = index;
   head->next = NULL;
   return head;
 }
 
-void push_node(struct Node *head) {
+void push_node(struct Node *head, int index) {
   struct Node *next = malloc(sizeof(struct Node));
-  next->index = -1;
   next->next = NULL;
+  next->index = index;
   while (head->next != NULL) {
     head = head->next;
   }
   assert(head->next == NULL);
 
-  next->index = head->index + 1;
-
   head->next = next;
 }
 
 int main(int argc, char **argv) {
-  struct Node *head = init_linked_list();
-  push_node(head);
-  push_node(head);
-  push_node(head);
+  struct Node *head = init_linked_list(0);
+  push_node(head, 1);
+  push_node(head, 2);
+  push_node(head, 3);
 
   assert(head->index == 0);
   printf("head index is %i\n", head->index);
@@ -42,9 +40,11 @@ int main(int argc, char **argv) {
   assert(head->index == 1);
   printf("head index is %i\n", head->index);
   head = head->next;
+
   assert(head->index == 2);
   printf("head index is %i\n", head->index);
   head = head->next;
+
   assert(head->index == 3);
   printf("head index is %i\n", head->index);
   head = head->next;
