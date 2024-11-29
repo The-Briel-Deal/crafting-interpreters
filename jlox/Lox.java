9ff37f66ac4aa9dc3eb4f63fa2d488c0f1431682
Gabriel Ford <gabe@fordltc.net>
2024-11-29 16:17:29 -0500

    Finished 4.1.0

--- jlox/Lox.java
+++ jlox/Lox.java
@@ -1,10 +1,53 @@
 package jlox;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class Lox {
-  public static void main(String[] args) throws IOException {
-    System.out.println("Hello Jlox");
-  }
+	public static void main(String[] args) throws IOException {
+		if (args.length > 1) {
+			System.out.println("Usage: jlox [script]");
+			System.exit(64);
+		} else if (args.length == 1) {
+			var filepath = args[0];
+			runFile(filepath);
+		} else {
+			runPrompt();
 
+		}
+	}
+
+	private static void runFile(String path) throws IOException {
+		var bytes = Files.readAllBytes(Paths.get(path));
+		run(new String(bytes, Charset.defaultCharset()));
+	}
+
+	private static void runPrompt() throws IOException {
+		var input = new InputStreamReader(System.in);
+		var reader = new BufferedReader(input);
+
+		for (;;) {
+			System.out.print("> ");
+			var line = reader.readLine();
+			if (line == null)
+				break;
+			run(line);
+		}
+	}
+
+	private static void run(String source) {
+		var scanner = new Scanner(source);
+		/**
+		 * TODO: scanTokens doesn't seem to implemented yet?
+		 * var tokens = scanner.scanTokens();
+		 * for Token token : tokens {
+		 * System.out.println(token);
+		 * }
+		 **/
+	}
 }
