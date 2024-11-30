9e982f685b356ade4b9d0c1d62066a2789b59172
Gabriel Ford <gabe@fordltc.net>
2024-11-30 01:22:09 -0500

    Scanner/lexer is done!

--- jlox/Scanner.java
+++ jlox/Scanner.java
@@ -1,10 +1,43 @@
 package jlox;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 import static jlox.TokenType.*;
 
 class Scanner {
+
+	private static final Map<String, TokenType> keywords;
+
+	static {
+		keywords = new HashMap<>();
+		// I don't want to figure out how to turn off formatting here lol.
+		// @formatter:off
+		// nvm, i figured it out, this little guy I made got smooshed with it on ):
+		//                     
+		//                    ^^-^^^-^ 
+		//                   ////\\\\\\   < Hi i'm a little guy with a tie!
+		keywords.put("and", /* o   o */ AND);
+		keywords.put("class", /*   */ CLASS);
+		keywords.put("else", /*     */ ELSE);
+		keywords.put("false", /* \/*/ FALSE);
+		keywords.put("for", /*   /\  */ FOR);
+		keywords.put("fun", /*  | |  */ FUN);
+		keywords.put("if", /*  \| |/  */ IF);
+		keywords.put("nil", /* /\/ \ */ NIL);
+		keywords.put("or", /*__\   /__*/ OR);
+		keywords.put("print", /*   */ PRINT);
+		keywords.put("return", /* */ RETURN);
+		keywords.put("super", /*/ \*/ SUPER);
+		keywords.put("this", /*|   \*/ THIS);
+		keywords.put("true", /*|   |*/ TRUE);
+		keywords.put("var", /* |   | */ VAR);
+		keywords.put("while", /*\ /*/ WHILE);
+		// @formatter:on
+	}
+
 	private final String source;
 	private final List<Token> tokens = new ArrayList<Token>();
 	private int start = 0;
@@ -73,8 +106,10 @@ class Scanner {
 				addToken(match('=') ? GREATER_EQUAL : GREATER);
 			case '/':
 				if (match('/')) {
-					while (peek() != '\n' && !isAtEnd())
+					while (peek() != '\n' && !isAtEnd()) {
 						advance();
+					}
+
 				} else {
 					addToken(SLASH);
 				}
@@ -87,17 +122,81 @@ class Scanner {
 			case '\n':
 				line++;
 				break;
+			case '"':
+				string();
+				break;
 			default:
-				Lox.error(line, "Unexpected character.");
+				if (isDigit(c)) {
+					number();
+				} else if (isAlpha(c)) {
+					identifier();
+				} else {
+					Lox.error(line, "Unexpected character.");
+				}
 				break;
 		}
 
+	}
+
+	private void string() {
+		while (peek() != '"' && !isAtEnd()) {
+			if (peek() == '\n') {
+				line++;
+			}
+			advance();
+		}
+		// File ended before end of string.
+		if (isAtEnd()) {
+			Lox.error(line, "Unterminated string.");
+			return;
+		}
+		// String has been closed.
+		advance();
+		// Trim the surrounding quotes.
+		String value = source.substring(start + 1, current - 1);
+		addToken(STRING, value);
+	}
+
+	private void number() {
+		while (isDigit(peek())) {
+			advance();
+		}
+		// Is the number fractional?
+		if (peek() == '.' && isDigit(peekNext())) {
+			// Consume the '.'.
+			advance();
+			while (isDigit(peek())) {
+				advance();
+			}
+		}
+
+		addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
+	}
+
+	private void identifier() {
+		while (isAlphaNumeric(peek())) {
+			advance();
+		}
+		String text = source.substring(start, current);
+		TokenType type = keywords.get(text);
+		if (type == null) {
+			type = IDENTIFIER;
+		}
+
+		addToken(type);
 	}
 
 	private char peek() {
 		if (isAtEnd())
 			return '\0';
 		return source.charAt(current);
+	}
+
+	private char peekNext() {
+		if (current + 1 >= source.length()) {
+			return '\0';
+		}
+		return source.charAt(current + 1);
 	}
 
 	private boolean match(char expected) {
@@ -124,8 +223,19 @@ class Scanner {
 		return source.charAt(current++);
 	}
 
+	private boolean isAlphaNumeric(char c) {
+		return isAlpha(c) || isDigit(c);
+	}
+
+	private boolean isAlpha(char c) {
+		return (c >= 'a' && c <= 'z') || (c >= 'a' && c <= 'z') || c == '_';
+	}
+
+	private boolean isDigit(char c) {
+		return c >= '0' && c <= '9';
+	}
+
 	private boolean isAtEnd() {
 		return current >= source.length();
-
 	}
 }
