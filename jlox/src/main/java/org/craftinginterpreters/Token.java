cfc0b5eaad381f1b59087f638ee0602ba900a9a0
Gabriel Ford <gabe@fordltc.net>
2024-11-30 22:46:05 -0500

    Added a test on the scanner.

--- jlox/src/main/java/org/craftinginterpreters/Token.java
+++ jlox/src/main/java/org/craftinginterpreters/Token.java
@@ -1,19 +1,34 @@
 package org.craftinginterpreters;
 
+import java.util.Objects;
+
 class Token {
-	final TokenType type;
-	final int line;
-	final Object literal;
-	final String lexeme;
+    final TokenType type;
+    final int line;
+    final Object literal;
+    final String lexeme;
 
-	Token(TokenType type, String lexeme, Object literal, int line) {
-		this.type = type;
-		this.line = line;
-		this.literal = literal;
-		this.lexeme = lexeme;
-	}
+    Token(TokenType type, String lexeme, Object literal, int line) {
+        this.type = type;
+        this.line = line;
+        this.literal = literal;
+        this.lexeme = lexeme;
+    }
 
-	public String toString() {
-		return String.format("%s %s %s", type, lexeme, literal);
-	}
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof Token token) {
+            return Objects.equals(this.lexeme, token.lexeme)
+                    && Objects.equals(this.literal, token.literal)
+                    && this.type == token.type
+                    && this.line == token.line;
+        }
+        return false;
+
+
+    }
+
+    public String toString() {
+        return String.format("%s %s %s", type, lexeme, literal);
+    }
 }
