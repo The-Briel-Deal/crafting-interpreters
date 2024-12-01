25124b5da8e8163d9b1544ee6e13c7d1a8b990aa
Gabriel Ford <gabe@fordltc.net>
2024-12-01 13:06:10 -0500

    Added a test on the scanner.

---
+++ jlox/src/test/java/org/craftinginterpreters/ScannerTest.java
@@ -0,0 +1,28 @@
+package org.craftinginterpreters;
+
+import org.junit.jupiter.api.Assertions;
+
+class ScannerTest {
+
+    @org.junit.jupiter.api.Test
+    void scanTokens() {
+        var scanner = new Scanner("if (foo == bar) {print \"Foo is bar\"}");
+        var tokens = scanner.scanTokens();
+        var result = tokens.toArray();
+        Token[] expect = {
+                new Token(TokenType.IF, "if", null, 1),
+                new Token(TokenType.LEFT_PAREN, "(", null, 1),
+                new Token(TokenType.IDENTIFIER, "foo", null, 1),
+                new Token(TokenType.EQUAL_EQUAL, "==", null, 1),
+                new Token(TokenType.IDENTIFIER, "bar", null, 1),
+                new Token(TokenType.RIGHT_PAREN, ")", null, 1),
+                new Token(TokenType.LEFT_BRACE, "{", null, 1),
+                new Token(TokenType.PRINT, "print", null, 1),
+                new Token(TokenType.STRING, "\"Foo is bar\"", "Foo is bar", 1),
+                new Token(TokenType.RIGHT_BRACE, "}", null, 1),
+                new Token(TokenType.EOF, "", null, 1),
+        };
+
+        Assertions.assertArrayEquals(expect, result);
+    }
+}
