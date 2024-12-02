c28e171c3aaba44ca6cf795f458f7a8c73c6cde4
gabe <gabe@fordltc.net>
2024-12-01 20:59:18 -0500

    Generating Expression type defs

--- jlox/src/main/java/tool/GenerateAst.java
+++ jlox/src/main/java/tool/GenerateAst.java
@@ -1,6 +1,8 @@
 package tool;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 public class GenerateAst {
@@ -10,13 +12,50 @@ public class GenerateAst {
             System.exit(64);
         }
         String outputDir = args[0];
-        defineAst(outputDir, "Expr",
-                List.of(
-                        "Binary   : Expr left, Token operator, Expr right",
-                        "Grouping : Expr expression",
-                        "Literal  : Object value",
-                        "Unary    : Token operator, Expr right",
-                        )
+        defineAst(outputDir, "Expr", List.of(
+                "Binary   : Expr left, Token operator, Expr right",
+                "Grouping : Expr expression",
+                "Literal  : Object value",
+                "Unary    : Token operator, Expr right")
         );
+    }
+
+    private static void defineAst(String outputDir, String baseName, List<String> types) throws IOException {
+        String path = outputDir + "/" + baseName + ".java";
+        PrintWriter writer = new PrintWriter(path, StandardCharsets.UTF_8);
+
+        writer.println("package org.craftinginterpreters.lox;");
+        writer.println();
+        writer.println("import java.util.List;");
+        writer.println();
+        writer.println("abstract class " + baseName + " {");
+        for (String type : types) {
+            String className = type.split(":")[0].trim();
+            String fields = type.split(":")[1].trim();
+            defineType(writer, baseName, className, fields);
+        }
+
+        writer.println("}");
+        writer.close();
+
+    }
+
+    private static void defineType(PrintWriter writer, String baseName, String className, String fieldList) {
+        writer.println("  static class " + className + " extends " + baseName + "{");
+        writer.println("    " + className + "(" + fieldList + ") {");
+
+        var fields = fieldList.split(", ");
+        for (var field : fields) {
+            var name = field.split(" ")[1];
+            writer.println("      this." + name + " = " + name + ";");
+        }
+        writer.println("    }");
+        writer.println();
+        for (var field : fields) {
+            writer.println("    final " + field + ";");
+        }
+        writer.println("  }");
+
+
     }
 }
