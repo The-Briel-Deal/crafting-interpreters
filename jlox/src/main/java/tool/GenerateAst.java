bc45d97c38e21d8eb1468899071e7c0ef06026a7
Gabriel Ford <gabe@fordltc.net>
2024-12-08 18:27:41 -0500

    Added global variables.

--- jlox/src/main/java/tool/GenerateAst.java
+++ jlox/src/main/java/tool/GenerateAst.java
@@ -16,11 +16,12 @@ public class GenerateAst {
 		defineAst(outputDir, "Expr",
 				List.of("Binary   : Expr left, Token operator, Expr right",
 						"Grouping : Expr expression", "Literal  : Object value",
-						"Unary    : Token operator, Expr right"));
+						"Unary    : Token operator, Expr right",
+						"Variable : Token name"));
 		defineAst(outputDir, "Stmt", Arrays.asList(
 				"Expression : Expr expression",
-				"Print      : Expr expression"
-		));
+				"Print      : Expr expression",
+				"Var        : Token name, Expr initializer"));
 	}
 
 	private static void defineAst(String outputDir, String baseName, List<String> types) throws IOException {
