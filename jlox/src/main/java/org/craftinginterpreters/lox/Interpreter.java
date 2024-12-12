d9170339d0bc0244d5447d94692191d8ac0ab82f
Gabriel Ford <gabe@fordltc.net>
2024-12-12 15:28:39 -0500

    Finished Logical Expressions.

--- jlox/src/main/java/org/craftinginterpreters/lox/Interpreter.java
+++ jlox/src/main/java/org/craftinginterpreters/lox/Interpreter.java
@@ -6,6 +6,7 @@ import org.craftinginterpreters.lox.Expr.Assign;
 import org.craftinginterpreters.lox.Expr.Binary;
 import org.craftinginterpreters.lox.Expr.Grouping;
 import org.craftinginterpreters.lox.Expr.Literal;
+import org.craftinginterpreters.lox.Expr.Logical;
 import org.craftinginterpreters.lox.Expr.Unary;
 import org.craftinginterpreters.lox.Stmt.Block;
 import org.craftinginterpreters.lox.Stmt.If;
@@ -51,6 +52,21 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 	@Override
 	public Object visitLiteralExpr(Literal expr) {
 		return expr.value;
+	}
+
+	@Override
+	public Object visitLogicalExpr(Logical expr) {
+		Object left = evaluate(expr.left);
+
+		if (expr.operator.type == TokenType.OR) {
+			if (isTruthy(left))
+				return left;
+		} else {
+			if (!isTruthy(left))
+				return left;
+		}
+
+		return evaluate(expr.right);
 	}
 
 	@Override
