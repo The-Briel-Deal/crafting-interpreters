010095c6a24514dd26ba731707825644a322da5b
Gabriel Ford <gabe@fordltc.net>
2024-12-13 12:43:34 -0500

    Add Lox functions, impl a native time func.

--- jlox/src/main/java/org/craftinginterpreters/lox/Interpreter.java
+++ jlox/src/main/java/org/craftinginterpreters/lox/Interpreter.java
@@ -1,9 +1,11 @@
 package org.craftinginterpreters.lox;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.craftinginterpreters.lox.Expr.Assign;
 import org.craftinginterpreters.lox.Expr.Binary;
+import org.craftinginterpreters.lox.Expr.Call;
 import org.craftinginterpreters.lox.Expr.Grouping;
 import org.craftinginterpreters.lox.Expr.Literal;
 import org.craftinginterpreters.lox.Expr.Logical;
@@ -12,7 +14,29 @@ import org.craftinginterpreters.lox.Stmt.Block;
 import org.craftinginterpreters.lox.Stmt.If;
 
 class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
-	private Environment environment = new Environment();
+	final Environment globals = new Environment();
+	private Environment environment = globals;
+
+	Interpreter() {
+		globals.define("clock", new LoxCallable() {
+			@Override
+			public int arity() {
+				return 0;
+			}
+
+			@Override
+			public Object call(Interpreter interpreter, List<Object> arguments) {
+				return (double) System.currentTimeMillis() / 1000.0;
+			}
+
+			@Override
+			public String toString() {
+				return "<native fn>";
+			}
+
+		});
+
+	}
 
 	void interpret(List<Stmt> statements) {
 		try {
@@ -130,6 +154,29 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 			default:
 				return null;
 		}
+	}
+
+	@Override
+	public Object visitCallExpr(Call expr) {
+		Object callee = evaluate(expr.callee);
+
+		List<Object> arguments = new ArrayList<>();
+		for (Expr argument : expr.arguments) {
+			arguments.add(evaluate(argument));
+		}
+
+		if (!(callee instanceof LoxCallable)) {
+			throw new RuntimeError(expr.paren, "Can only call functions and classes.");
+		}
+
+		LoxCallable function = (LoxCallable) callee;
+		if (arguments.size() != function.arity()) {
+			throw new RuntimeError(expr.paren,
+					"Expected " + function.arity() + " arguments but got " + arguments.size() + ".");
+
+		}
+
+		return function.call(this, arguments);
 	}
 
 	private Object evaluate(Expr expr) {
