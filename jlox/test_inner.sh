801f05e4817bf0cd17c4a327e605f352b15e804e
Gabriel Ford <gabe@fordltc.net>
2025-01-04 14:24:33 -0500

    Added test script.

---
+++ jlox/test_inner.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+RESULT="$(java -cp ./target/classes/ org.craftinginterpreters.lox.Lox ./scripts/inner.lox)"
+EXPECT="Fry until golden brown.
+Pipe full of custard and coat with chocolate.
+Place in a nice box.
+"
+
+if [[ "$EXPECT" == "$RESULT" ]]; then
+	echo "PASS!"
+else
+	echo "FAILED"
+	echo ""
+	echo "Expected:"
+	echo ">$EXPECT<"
+	echo ""
+	echo "Result:"
+	echo ">$RESULT<"
+fi
