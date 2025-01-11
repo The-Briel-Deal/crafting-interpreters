7242d240aa6fe43aa751b2673fbfd18e059bb655
Gabriel Ford <gabe@fordltc.net>
2025-01-11 12:52:16 -0500

    Finish chapter one.

--- clox/include/chunk.h
+++ clox/include/chunk.h
@@ -5,7 +5,7 @@
 #include "value.h"
 
 typedef enum {
-	OP_CONSTANT,
+  OP_CONSTANT,
   OP_RETURN,
 } OpCode;
 
@@ -13,12 +13,13 @@ typedef struct {
   int count;
   int capacity;
   uint8_t *code;
+  int *lines;
   ValueArray constants;
 } Chunk;
 
 void initChunk(Chunk *chunk);
 void freeChunk(Chunk *chunk);
-void writeChunk(Chunk *chunk, uint8_t byte);
+void writeChunk(Chunk *chunk, uint8_t byte, int line);
 
 /**
  * Adds a constant to a chunk and returns the index.
--- clox/src/main.c
+++ clox/src/main.c
@@ -6,13 +6,12 @@ int main(int argc, char *argv[]) {
   Chunk chunk;
   initChunk(&chunk);
 
-	int constant = addConstant(&chunk, 1.2);
-	writeChunk(&chunk, OP_CONSTANT);
-	writeChunk(&chunk, constant);
+  int constant = addConstant(&chunk, 1.2);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
 
-
-  writeChunk(&chunk, OP_RETURN);
-	disassembleChunk(&chunk, "test chunk");
+  writeChunk(&chunk, OP_RETURN, 123);
+  disassembleChunk(&chunk, "test chunk");
   freeChunk(&chunk);
   return 0;
 }
