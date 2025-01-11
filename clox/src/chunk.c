6f48966f5fcfa4b6765c42192acc521426d546cb
Gabriel Ford <gabe@fordltc.net>
2025-01-11 10:49:44 -0500

    Add constants to chunks.

--- clox/src/chunk.c
+++ clox/src/chunk.c
@@ -3,24 +3,42 @@
 
 #include "chunk.h"
 #include "memory.h"
+#include "value.h"
 
 void initChunk(Chunk *chunk) {
-	chunk->count = 0;
-	chunk->capacity = 0;
-	chunk->code = NULL;
+  chunk->count = 0;
+  chunk->capacity = 0;
+  chunk->code = NULL;
+  initValueArray(&chunk->constants);
 }
 
 void freeChunk(Chunk *chunk) {
   FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
+  freeValueArray(&chunk->constants);
+  initChunk(chunk);
 }
 
 void writeChunk(Chunk *chunk, uint8_t byte) {
-	if (chunk->capacity < chunk->count + 1) {
-		int oldCapacity = chunk->capacity;
-		chunk->capacity = GROW_CAPACITY(oldCapacity);
-		chunk->code = GROW_ARRAY(uint8_t, chunk->code, oldCapacity, chunk->capacity);
-	}
+  if (chunk->capacity < chunk->count + 1) {
+    int oldCapacity = chunk->capacity;
+    chunk->capacity = GROW_CAPACITY(oldCapacity);
+    chunk->code =
+        GROW_ARRAY(uint8_t, chunk->code, oldCapacity, chunk->capacity);
+  }
 
-	chunk->code[chunk->count] = byte;
-	chunk->count++;
+  chunk->code[chunk->count] = byte;
+  chunk->count++;
+}
+
+/**
+ * Adds a constant to a chunk and returns the index.
+ *
+ * @param chunk Chunk to add to.
+ * @param value Value to add to the afformentioned chunk.
+ * @return The index to reference this constant later.
+ */
+int addConstant(Chunk *chunk, Value value) {
+  writeValueArray(&chunk->constants, value);
+  // Return index of constant.
+  return chunk->constants.count - 1;
 }
