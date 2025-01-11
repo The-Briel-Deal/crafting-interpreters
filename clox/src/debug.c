61540544e53c6501a236db67de296622466ab5c1
Gabriel Ford <gabe@fordltc.net>
2025-01-11 11:23:16 -0500

    Add constants to disassembler.

--- clox/src/debug.c
+++ clox/src/debug.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
+#include "chunk.h"
 #include "debug.h"
-
+#include "value.h"
 
 void disassembleChunk(Chunk *chunk, const char *name) {
   printf("== %s ==\n", name);
@@ -11,9 +12,26 @@ void disassembleChunk(Chunk *chunk, const char *name) {
   }
 }
 
+/**
+ * Prints a constantInstruction with its value given the chunk and offset.
+ *
+ * @param name The name of the constant instruction, most likely 'OP_CONSTANT'.
+ * @param chunk Pointer to the chunk the constant resides in.
+ * @param offset The offset of this instruction in the chunk.
+ *
+ * @return The offset of the next instruction.
+ */
+static int constantInstruction(const char *name, Chunk *chunk, int offset) {
+  uint8_t constant = chunk->code[offset + 1];
+  printf("%-16s %4d '", name, constant);
+  printValue(chunk->constants.values[constant]);
+  printf("'\n");
+  return offset + 2;
+}
+
 static int simpleInstruction(const char *name, int offset) {
   printf("%s\n", name);
-	return offset + 1;
+  return offset + 1;
 }
 
 int disassembleInstruction(Chunk *chunk, int offset) {
@@ -21,6 +39,8 @@ int disassembleInstruction(Chunk *chunk, int offset) {
 
   uint8_t instruction = chunk->code[offset];
   switch (instruction) {
+  case OP_CONSTANT:
+    return constantInstruction("OP_CONSTANT", chunk, offset);
   case OP_RETURN:
     return simpleInstruction("OP_RETURN", offset);
   default:
