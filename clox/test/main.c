6d489a6ae8f39a10b06dec9ac230473c490b50cd
Gabriel Ford <gabe@fordltc.net>
2025-01-12 15:48:38 -0500

    Get started on capturing test output.

--- clox/test/main.c
+++ clox/test/main.c
@@ -1,3 +1,23 @@
 #include <stdio.h>
+#include <unistd.h>
 
-int main(int argc, char *argv[]) { printf("Starting Tests (:"); }
+static int testWriteChunk();
+int main(int argc, char *argv[]) {
+  printf("Starting Tests (:\n");
+  testWriteChunk();
+  printf("Tests Succeeded!\n");
+}
+
+static int testWriteChunk() {
+  FILE *prev_stdout = stdout;
+  char buf[1000];
+  FILE *membufstdout = fmemopen(buf, sizeof(buf), "w");
+  printf("Start position of mem buffer: %ld\n", ftell(membufstdout));
+  stdout = membufstdout;
+  printf("Cute Beans");
+  long end_pos = ftell(membufstdout);
+  fclose(stdout);
+  stdout = prev_stdout;
+  printf("End position of mem buffer: %ld\n", end_pos);
+  return 0;
+}
