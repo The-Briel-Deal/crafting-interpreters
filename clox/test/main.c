b8b725f0d8f23e7c4fa1588ee9b248e046efa303
Gabriel Ford <gabe@fordltc.net>
2025-01-12 17:37:34 -0500

    Generate debug symbols and finish first test.

--- clox/test/main.c
+++ clox/test/main.c
@@ -1,23 +1,74 @@
+#include <assert.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
-static int testWriteChunk();
+#include "chunk.h"
+#include "debug.h"
+
+static bool runTest(char *testName, void (*testCase)(), char *expect);
+
+static void testWriteChunk();
+
 int main(int argc, char *argv[]) {
   printf("Starting Tests (:\n");
-  testWriteChunk();
+
+  char testWriteChunkExpect[] = "== test chunk ==\n"
+                                "0000  123 OP_CONSTANT         0 '1.2'\n"
+                                "0002    | OP_RETURN\n";
+
+  assert(runTest("testWriteChunk", testWriteChunk, testWriteChunkExpect));
   printf("Tests Succeeded!\n");
 }
 
-static int testWriteChunk() {
-  FILE *prev_stdout = stdout;
-  char buf[1000];
+static void testWriteChunk() {
+  Chunk chunk;
+  initChunk(&chunk);
+
+  int constant = addConstant(&chunk, 1.2);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
+
+  writeChunk(&chunk, OP_RETURN, 123);
+  disassembleChunk(&chunk, "test chunk");
+  freeChunk(&chunk);
+}
+
+/***
+ * Takes in a functionPtr test case and checks if the stdout output is equal to
+ * the expect string.
+ *
+ * @param testName The name of the test being run.
+ * @param testCase A functionPtr which should print output (ideally) equal to
+ * the expect param.
+ * @param expect A string which is expected to equal the output of the testCase.
+ *
+ * @return Whether or not the test passed or failed.
+ */
+static bool runTest(char *testName, void (*testCase)(), char *expect) {
+  char buf[1024];
   FILE *membufstdout = fmemopen(buf, sizeof(buf), "w");
-  printf("Start position of mem buffer: %ld\n", ftell(membufstdout));
+
+  FILE *prev_stdout = stdout;
   stdout = membufstdout;
-  printf("Cute Beans");
+
+  testCase();
+
   long end_pos = ftell(membufstdout);
   fclose(stdout);
+
   stdout = prev_stdout;
-  printf("End position of mem buffer: %ld\n", end_pos);
-  return 0;
+
+  buf[end_pos] = '\0';
+  char *result = buf;
+
+  if (strcmp(result, expect)) {
+    printf("Test '%s' failed.\nExpect: '%s'\nResult: '%s'", testName, expect,
+           result);
+
+    return false;
+  }
+
+  return true;
 }
