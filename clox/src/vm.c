e7064841c0d551428cc4d049c493fdcc39ec21e1
Gabriel Ford <gabe@fordltc.net>
2025-01-19 12:13:40 -0500

    Finish 15.2.1: The VM's stack

--- clox/src/vm.c
+++ clox/src/vm.c
@@ -1,20 +1,36 @@
 #include <stdio.h>
 
 #include "chunk.h"
+#include "debug.h"
 #include "value.h"
 #include "vm.h"
 
 VM vm;
 
-void initVM() {}
+static void resetStack() { vm.stackTop = vm.stack; };
+
+void initVM() { resetStack(); }
 
 void freeVM() {}
+
+void push(Value value) {
+  *vm.stackTop = value;
+  vm.stackTop++;
+}
+
+Value pop() {
+  vm.stackTop--;
+  return *vm.stackTop;
+}
 
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
 
   for (;;) {
+#ifdef DEBUG_TRACE_EXECUTION
+    disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
+#endif
     uint8_t instruction;
     switch (instruction = READ_BYTE()) {
     case OP_RETURN: {
