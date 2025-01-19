d97930fba2784099772cac283f530d8a75149ab5
Gabriel Ford <gabe@fordltc.net>
2025-01-19 13:13:02 -0500

    Added common arithmetic operations, and test them in main.

--- clox/include/chunk.h
+++ clox/include/chunk.h
@@ -7,7 +7,11 @@
 
 typedef enum {
   OP_CONSTANT,
-	OP_NEGATE,
+  OP_ADD,
+  OP_SUBTRACT,
+  OP_MULTIPLY,
+  OP_DIVIDE,
+  OP_NEGATE,
   OP_RETURN,
 } OpCode;
 
--- clox/src/main.c
+++ clox/src/main.c
@@ -8,14 +8,33 @@ int main(int argc, char *argv[]) {
   Chunk chunk;
   initChunk(&chunk);
 
+  // Constant 1 = 1.2
   int constant = addConstant(&chunk, 1.2);
   writeChunk(&chunk, OP_CONSTANT, 123);
   writeChunk(&chunk, constant, 123);
-	writeChunk(&chunk, OP_NEGATE, 123);
+
+  // Constant 2 = 3.4
+  constant = addConstant(&chunk, 3.4);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
+
+  // Pops Const 1 and Const 2, then pushes the sum of 4.6
+  writeChunk(&chunk, OP_ADD, 123);
+
+  // Constant 3 = 5.6
+  constant = addConstant(&chunk, 5.6);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
+
+  // (C1 + C2) / C3 = .821~ish
+  writeChunk(&chunk, OP_DIVIDE, 123);
+
+	// -((C1 + C2) / C3) = -.821~ish
+  writeChunk(&chunk, OP_NEGATE, 123);
 
   writeChunk(&chunk, OP_RETURN, 123);
   disassembleChunk(&chunk, "test chunk");
-	interpret(&chunk);
+  interpret(&chunk);
   freeVM();
   freeChunk(&chunk);
   return 0;
