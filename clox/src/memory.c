604c26d4a95b400e3fc72520dd10a27c89d70bb9
Gabriel Ford <gabe@fordltc.net>
2025-01-18 20:06:00 -0500

    Write my own impl of realloc and free.

--- clox/src/memory.c
+++ clox/src/memory.c
@@ -1,6 +1,33 @@
+#include <stdbool.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 #include "memory.h"
+
+typedef struct {
+  int size;
+	bool freed;
+} MemChunkMetadata;
+
+void *realloc(void *__ptr, size_t __size) {
+  MemChunkMetadata *p = sbrk(__size + sizeof(MemChunkMetadata));
+  p->size = __size;
+  void *newMemPtr = p + 1;
+
+  if (__ptr) {
+    MemChunkMetadata *oldMetadata = ((MemChunkMetadata *)__ptr) - 1;
+    int oldSize = oldMetadata->size;
+    memcpy(newMemPtr, __ptr, oldSize);
+  }
+
+	return newMemPtr;
+}
+
+void free (void *__ptr) {
+	(((MemChunkMetadata*)__ptr)-1)->freed = true;
+};
 
 void *reallocate(void *pointer, size_t oldSize, size_t newSize) {
   if (newSize == 0) {
