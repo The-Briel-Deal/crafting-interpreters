756df99292819f9f30b922ae2d7dd8cf617ebe7a
Gabriel Ford <gabe@fordltc.net>
2025-01-19 22:40:19 -0500

    Finish 16.2 - Created basic token parsing tooling.

--- clox/src/scanner.c
+++ clox/src/scanner.c
@@ -1,3 +1,7 @@
+#include <stdbool.h>
+#include <string.h>
+
+#include "scanner.h"
 
 typedef struct {
   const char *start;
@@ -11,4 +15,31 @@ void initScanner(const char *source) {
   scanner.start = source;
   scanner.current = source;
   scanner.line = 1;
+}
+
+static bool isAtEnd() { return *scanner.current == '\0'; }
+
+static Token makeToken(TokenType type) {
+  Token token;
+  token.type = type;
+  token.start = scanner.start;
+  token.length = (int)(scanner.current - scanner.start);
+  token.line = scanner.line;
+  return token;
+}
+
+static Token errorToken(const char* message ) {
+	Token token;
+	token.type = TOKEN_ERROR;
+	token.start = message;
+	token.length = (int) strlen(message);
+	return token;
+}
+
+Token scanToken() {
+  scanner.start = scanner.current;
+  if (isAtEnd())
+    return makeToken(TOKEN_EOF);
+
+  return errorToken("Unexpected character.");
 }
