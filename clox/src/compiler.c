4ca972360355d1189803eb1b7e41ad6d017bc345
Gabriel Ford <gabe@fordltc.net>
2025-01-26 11:36:55 -0500

    17.1 - Create advance func, and Parser struct.

--- clox/src/compiler.c
+++ clox/src/compiler.c
@@ -3,6 +3,24 @@
 #include "chunk.h"
 #include "scanner.h"
 
+typedef struct {
+  Token current;
+  Token previous;
+} Parser;
+
+Parser parser;
+
+static void advance() {
+  parser.previous = parser.current;
+  for (;;) {
+    parser.current = scanToken();
+    if (parser.current.type != TOKEN_ERROR)
+      break;
+
+    errorAtCurrent(parser.current.start);
+  }
+}
+
 void compile(const char *source, Chunk *chunk) {
   initScanner(source);
   advance();
