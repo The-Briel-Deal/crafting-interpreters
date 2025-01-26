452ff4e906b01b10d15d2e21961bb05bc884ceba
Gabriel Ford <gabe@fordltc.net>
2025-01-26 17:26:33 -0500

    Add rules table, and update clang format.

--- clox/src/compiler.c
+++ clox/src/compiler.c
@@ -28,6 +28,14 @@ typedef enum {
   PREC_PRIMARY,
 } Precedence;
 
+typedef void (*ParseFn)();
+
+typedef struct {
+  ParseFn prefix;
+  ParseFn infix;
+  Precedence precedence;
+} ParseRule;
+
 Parser parser;
 Chunk *compilingChunk;
 static Chunk *currentChunk() {
@@ -113,7 +121,7 @@ static void endCompiler() {
 
 static void binary() {
   TokenType operatorType = parser.previous.type;
-  ParseRule *rule = getRule(operatorType);
+  ParseRule *rule        = getRule(operatorType);
   parsePrecedence((Precedence)(rule->precedence + 1));
 
   switch (operatorType) {
@@ -158,6 +166,49 @@ static void unary() {
   }
 }
 
+ParseRule rules[] = {
+    [TOKEN_LEFT_PAREN]    = {grouping, NULL,   PREC_NONE  },
+    [TOKEN_RIGHT_PAREN]   = {NULL,     NULL,   PREC_NONE  },
+    [TOKEN_LEFT_BRACE]    = {NULL,     NULL,   PREC_NONE  },
+    [TOKEN_RIGHT_BRACE]   = {NULL,     NULL,   PREC_NONE  },
+    [TOKEN_COMMA]         = {NULL,     NULL,   PREC_NONE  },
+    [TOKEN_DOT]           = {NULL,     NULL,   PREC_NONE  },
+    [TOKEN_MINUS]         = {unary,    binary, PREC_TERM  },
+    [TOKEN_PLUS]          = {NULL,     binary, PREC_TERM  },
+    [TOKEN_SEMICOLON]     = {NULL,     NULL,   PREC_NONE  },
+    [TOKEN_SLASH]         = {NULL,     binary, PREC_FACTOR},
+    [TOKEN_STAR]          = {NULL,     binary, PREC_FACTOR},
+    [TOKEN_BANG]          = {NULL,     NULL,   PREC_NONE  },
+    [TOKEN_BANG_EQUAL]    = {NULL,     NULL,   PREC_NONE  },
+    [TOKEN_EQUAL]         = {NULL,     NULL,   PREC_NONE  },
+    [TOKEN_EQUAL_EQUAL]   = {NULL,     NULL,   PREC_NONE  },
+    [TOKEN_GREATER]       = {NULL,     NULL,   PREC_NONE  },
+    [TOKEN_GREATER_EQUAL] = {NULL,     NULL,   PREC_NONE  },
+    [TOKEN_LESS]          = {NULL,     NULL,   PREC_NONE  },
+    [TOKEN_LESS_EQUAL]    = {NULL,     NULL,   PREC_NONE  },
+    [TOKEN_IDENTIFIER]    = {NULL,     NULL,   PREC_NONE  },
+    [TOKEN_STRING]        = {NULL,     NULL,   PREC_NONE  },
+    [TOKEN_NUMBER]        = {number,   NULL,   PREC_NONE  },
+    [TOKEN_AND]           = {NULL,     NULL,   PREC_NONE  },
+    [TOKEN_CLASS]         = {NULL,     NULL,   PREC_NONE  },
+    [TOKEN_ELSE]          = {NULL,     NULL,   PREC_NONE  },
+    [TOKEN_FALSE]         = {NULL,     NULL,   PREC_NONE  },
+    [TOKEN_FOR]           = {NULL,     NULL,   PREC_NONE  },
+    [TOKEN_FUN]           = {NULL,     NULL,   PREC_NONE  },
+    [TOKEN_IF]            = {NULL,     NULL,   PREC_NONE  },
+    [TOKEN_NIL]           = {NULL,     NULL,   PREC_NONE  },
+    [TOKEN_OR]            = {NULL,     NULL,   PREC_NONE  },
+    [TOKEN_PRINT]         = {NULL,     NULL,   PREC_NONE  },
+    [TOKEN_RETURN]        = {NULL,     NULL,   PREC_NONE  },
+    [TOKEN_SUPER]         = {NULL,     NULL,   PREC_NONE  },
+    [TOKEN_THIS]          = {NULL,     NULL,   PREC_NONE  },
+    [TOKEN_TRUE]          = {NULL,     NULL,   PREC_NONE  },
+    [TOKEN_VAR]           = {NULL,     NULL,   PREC_NONE  },
+    [TOKEN_WHILE]         = {NULL,     NULL,   PREC_NONE  },
+    [TOKEN_ERROR]         = {NULL,     NULL,   PREC_NONE  },
+    [TOKEN_EOF]           = {NULL,     NULL,   PREC_NONE  },
+};
+
 static void parsePrecedence(Precedence precedence) {
 }
 
@@ -169,7 +220,7 @@ bool compile(const char *source, Chunk *chunk) {
   initScanner(source);
   compilingChunk = chunk;
 
-  parser.hadError = false;
+  parser.hadError  = false;
   parser.panicMode = false;
 
   advance();
