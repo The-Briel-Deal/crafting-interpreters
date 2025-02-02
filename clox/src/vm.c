56d429ef06642cb694ac6e024a32a9b4a184e225
Gabriel Ford <gabe@fordltc.net>
2025-02-02 13:36:03 -0500

    Add some error handling for negating non-numbers.

--- clox/src/vm.c
+++ clox/src/vm.c
@@ -1,9 +1,12 @@
+#include <stdarg.h>
 #include <stdbool.h>
 #include <stdio.h>
 
 #include "chunk.h"
 #include "compiler.h"
+#ifdef DEBUG_TRACE_EXECUTION
 #include "debug.h"
+#endif
 #include "value.h"
 #include "vm.h"
 
@@ -12,6 +15,19 @@ VM vm;
 static void resetStack() {
   vm.stackTop = vm.stack;
 };
+
+static void runtimeError(const char *format, ...) {
+  va_list args;
+  va_start(args, format);
+  vfprintf(stderr, format, args);
+  va_end(args);
+  fputs("\n", stderr);
+
+  size_t instruction = vm.ip - vm.chunk->code - 1;
+  int    line        = vm.chunk->lines[instruction];
+  fprintf(stderr, "[line %d] in script\n", line);
+  resetStack();
+}
 
 void initVM() {
   resetStack();
@@ -28,6 +44,10 @@ void push(Value value) {
 Value pop() {
   vm.stackTop--;
   return *vm.stackTop;
+}
+
+static Value peek(int distance) {
+  return vm.stackTop[-1 - distance];
 }
 
 static InterpretResult run() {
@@ -71,7 +91,11 @@ static InterpretResult run() {
       BINARY_OP(/);
       break;
     case OP_NEGATE:
-      push(-pop());
+      if (!IS_NUMBER(peek(0))) {
+        runtimeError("Operand must be a number.");
+        return INTERPRET_RUNTIME_ERROR;
+      }
+      push(NUMBER_VAL(-AS_NUMBER(pop())));
       break;
     case OP_RETURN: {
       printValue(pop());
