ffa5d95db6e3b0ba0e9c7a1ee9739a92b8771ed5
Gabriel Ford <gabe@fordltc.net>
2025-02-02 16:41:21 -0500

    Add support for equals, greater, and less ops.

--- clox/src/value.c
+++ clox/src/value.c
@@ -28,8 +28,8 @@ void freeValueArray(ValueArray *array) {
 
 void printValue(Value value) {
   switch (value.type) {
-  case VAL_BOOL: printf(AS_BOOL(value) ? "true" : "false"); break;
-  case VAL_NIL: printf("nil"); break;
+  case VAL_BOOL  : printf(AS_BOOL(value) ? "true" : "false"); break;
+  case VAL_NIL   : printf("nil"); break;
   case VAL_NUMBER: printf("%g", AS_NUMBER(value)); break;
   }
 }
