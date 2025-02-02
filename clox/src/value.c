1f5ed3ce00864fefe90c6d92ebcd4eb729c4e8e4
Gabriel Ford <gabe@fordltc.net>
2025-02-02 18:15:54 -0500

    Finish Chap 18 - Types of Values

--- clox/src/value.c
+++ clox/src/value.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdio.h>
 
 #include "memory.h"
@@ -31,5 +32,16 @@ void printValue(Value value) {
   case VAL_BOOL  : printf(AS_BOOL(value) ? "true" : "false"); break;
   case VAL_NIL   : printf("nil"); break;
   case VAL_NUMBER: printf("%g", AS_NUMBER(value)); break;
+  }
+}
+
+bool valuesEqual(Value a, Value b) {
+  if (a.type != b.type)
+    return false;
+  switch (a.type) {
+  case VAL_BOOL  : return AS_BOOL(a) == AS_BOOL(b);
+  case VAL_NIL   : return true;
+  case VAL_NUMBER: return AS_NUMBER(a) == AS_NUMBER(b);
+  default        : assert(false); return false; // Should be unreachable.
   }
 }
