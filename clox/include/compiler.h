0d794a76c91ad2803b54754fb2d60658d8b23340
Gabriel Ford <gabe@fordltc.net>
2025-02-03 13:55:00 -0500

    Finish Chap 19.3 - Strings.

--- clox/include/compiler.h
+++ clox/include/compiler.h
@@ -1,9 +1,11 @@
 #ifndef clox_compiler_h
 #define clox_compiler_h
 
-#include "chunk.h"
-
 #include <stdbool.h>
+
+#include "chunk.h"
+#include "object.h"
+
 
 bool compile(const char *source, Chunk *chunk);
 
