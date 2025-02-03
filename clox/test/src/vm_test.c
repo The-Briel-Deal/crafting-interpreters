765c2e390946036bb5173af125e07083e6a6e992
Gabriel Ford <gabe@fordltc.net>
2025-02-03 17:03:32 -0500

    Add string cat test.

--- clox/test/src/vm_test.c
+++ clox/test/src/vm_test.c
@@ -38,12 +38,19 @@ void       testBooleanLogic2() {
   interpret(source);
 }
 
+const char TEST_STRING_CONCAT_1_EXPECT[] = "Boogie Woogie Woohoo\n";
+void       testStringConcat1() {
+  initVM();
+  char source[] = "\"Boogie \" + \"Woogie \" + \"Woohoo\"";
+  interpret(source);
+}
 const TestCase VM_TESTS[] = {
     {"testArithmetic1",   testArithmetic1,   TEST_ARITHMETIC_1_EXPECT   },
     {"testArithmetic2",   testArithmetic2,   TEST_ARITHMETIC_2_EXPECT   },
     {"testArithmetic3",   testArithmetic3,   TEST_ARITHMETIC_3_EXPECT   },
     {"testBooleanLogic1", testBooleanLogic1, TEST_BOOLEAN_LOGIC_1_EXPECT},
     {"testBooleanLogic2", testBooleanLogic2, TEST_BOOLEAN_LOGIC_2_EXPECT},
+    {"testStringConcat1", testStringConcat1, TEST_STRING_CONCAT_1_EXPECT},
 };
 
 const int VM_TESTS_COUNT = sizeof(VM_TESTS) / sizeof(TestCase);
