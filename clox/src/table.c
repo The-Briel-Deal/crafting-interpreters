d8c57dc21c30b1f693d4cff4cf7deae1ce94f531
Gabriel Ford <gabe@fordltc.net>
2025-02-04 00:10:26 -0500

    Finish Chap 20.4.3 - Resizing a Hash Table.

--- clox/src/table.c
+++ clox/src/table.c
@@ -23,8 +23,29 @@ static Entry *findEntry(Entry *entries, int capacity, ObjString *key) {
       return entry;
     }
 
-		index = (index + 1) % capacity;
+    index = (index + 1) % capacity;
   }
+}
+
+static void adjustCapacity(Table *table, int capacity) {
+  Entry *entries = ALLOCATE(Entry, capacity);
+  for (int i = 0; i < capacity; i++) {
+    entries[i].key   = NULL;
+    entries[i].value = NIL_VAL;
+  }
+
+  for (int i = 0; i < table->capacity; i++) {
+    Entry *entry = &table->entries[i];
+    if (entry->key == NULL)
+      continue;
+    Entry *dest = findEntry(entries, capacity, entry->key);
+    dest->key   = entry->key;
+    dest->value = entry->value;
+  }
+
+  FREE_ARRAY(Entry, table->entries, table->capacity);
+  table->entries  = entries;
+  table->capacity = capacity;
 }
 
 bool tableSet(Table *table, ObjString *key, Value value) {
@@ -40,4 +61,13 @@ bool tableSet(Table *table, ObjString *key, Value value) {
   entry->key   = key;
   entry->value = value;
   return isNewKey;
+}
+
+void tableAddAll(Table *from, Table *to) {
+  for (int i = 0; i < from->capacity; i++) {
+    Entry *entry = &from->entries[i];
+    if (entry->key != NULL) {
+      tableSet(to, entry->key, entry->value);
+    }
+  }
 }
