5df6ad124cbc59bee291c799a1ca8e189ce46a44
Gabriel Ford <gabe@fordltc.net>
2025-02-05 15:19:26 -0500

    Improve string concat benchmark.

--- clox/benchmark/src/main.c
+++ clox/benchmark/src/main.c
@@ -1,14 +1,44 @@
-#include "vm.h"
+#include <limits.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <time.h>
 
+#include "vm.h"
+
+#define BENCH_RUN_COUNT 2000
+
+long benchStrConcat();
+
+int main(int argc, char *argv[]) {
+  int results[BENCH_RUN_COUNT] = {0};
+  for (int i = 0; i < BENCH_RUN_COUNT; i++) {
+    results[i] = benchStrConcat();
+  }
+  int   max = INT_MIN;
+  int   min = INT_MAX;
+  float avg = 0;
+  for (int i = 0; i < BENCH_RUN_COUNT; i++) {
+    avg += results[i] / (float)BENCH_RUN_COUNT;
+    if (results[i] < min) {
+      min = results[i];
+    }
+    if (results[i] > max) {
+      max = results[i];
+    }
+  }
+
+  printf("benchStrConcat:\n\nMin: %i\nMax: %i\nAvg: %f\n", min, max, avg);
+}
+
 const char VERY_LONG_STR_CONCAT_SCRIPT[];
-int        main(int argc, char *argv[]) {
+long       benchStrConcat() {
   initVM();
   long startTime = clock();
   interpret(VERY_LONG_STR_CONCAT_SCRIPT);
-	long endTime = clock();
-	printf("Start Time: %li, End Time: %li, Elapsed Time: %li", startTime,  endTime, endTime - startTime);
+  long endTime = clock();
+  printf("\nStart Time: %li\nEnd Time: %li\nElapsed Time: %li\n", startTime,
+               endTime, endTime - startTime);
+  return endTime - startTime;
 }
 
 const char VERY_LONG_STR_CONCAT_SCRIPT[] =
