4e978110ee200c89c8a915086aadcf5b552586d5
Gabriel Ford <gabe@fordltc.net>
2025-02-05 15:43:32 -0500

    Redirect benchmark output to a file.

--- clox/benchmark/src/main.c
+++ clox/benchmark/src/main.c
@@ -8,32 +8,37 @@
 
 #define BENCH_RUN_COUNT 2000
 
-#define RUN_BENCH(benchName)                                                   \
-  do {                                                                         \
-    int results[BENCH_RUN_COUNT] = {0};                                        \
-    for (int i = 0; i < BENCH_RUN_COUNT; i++) {                                \
-      results[i] = benchName();                                                \
-    }                                                                          \
-    int   max = INT_MIN;                                                       \
-    int   min = INT_MAX;                                                       \
-    float avg = 0;                                                             \
-    for (int i = 0; i < BENCH_RUN_COUNT; i++) {                                \
-      avg += results[i] / (float)BENCH_RUN_COUNT;                              \
-      if (results[i] < min) {                                                  \
-        min = results[i];                                                      \
-      }                                                                        \
-      if (results[i] > max) {                                                  \
-        max = results[i];                                                      \
-      }                                                                        \
-    }                                                                          \
-                                                                               \
-    printf("benchStrConcat:\n\nMin: %i\nMax: %i\nAvg: %f\n", min, max, avg);   \
-  } while (false)
-
-long benchStrConcat();
+long        benchStrConcat();
+static void runBench(long (*benchFn)(), FILE *stdoutRedirect);
 
 int main(int argc, char *argv[]) {
-  RUN_BENCH(benchStrConcat);
+  FILE *stdoutRedirect = fopen("./benchmark_out.txt", "w");
+  runBench(benchStrConcat, stdoutRedirect);
+  fclose(stdoutRedirect);
+}
+
+static void runBench(long (*benchFn)(), FILE *stdoutRedirect) {
+  FILE *stdoutPrev             = stdout;
+  stdout                       = stdoutRedirect;
+  int results[BENCH_RUN_COUNT] = {0};
+  for (int i = 0; i < BENCH_RUN_COUNT; i++) {
+    results[i] = benchFn();
+  }
+  int   max = INT_MIN;
+  int   min = INT_MAX;
+  float avg = 0;
+  for (int i = 0; i < BENCH_RUN_COUNT; i++) {
+    avg += results[i] / (float)BENCH_RUN_COUNT;
+    if (results[i] < min) {
+      min = results[i];
+    }
+    if (results[i] > max) {
+      max = results[i];
+    }
+  }
+  stdout = stdoutPrev;
+
+  printf("benchStrConcat:\nMin: %i\nMax: %i\nAvg: %f\n", min, max, avg);
 }
 
 const char VERY_LONG_STR_CONCAT_SCRIPT[];
