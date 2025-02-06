3068e0dc5a08180c5636910a396238f8f3949624
Gabriel Ford <gabe@fordltc.net>
2025-02-05 19:12:22 -0500

    Add support for creating global variables.

--- clox/src/debug.c
+++ clox/src/debug.c
@@ -49,6 +49,8 @@ int disassembleInstruction(Chunk *chunk, int offset) {
     case OP_TRUE    : return simpleInstruction("OP_TRUE", offset);
     case OP_FALSE   : return simpleInstruction("OP_FALSE", offset);
     case OP_POP     : return simpleInstruction("OP_POP", offset);
+    case OP_DEFINE_GLOBAL:
+      return constantInstruction("OP_DEFINE_GLOBAL", chunk, offset);
     case OP_EQUAL   : return simpleInstruction("OP_EQUAL", offset);
     case OP_GREATER : return simpleInstruction("OP_GREATER", offset);
     case OP_ADD     : return simpleInstruction("OP_ADD", offset);
