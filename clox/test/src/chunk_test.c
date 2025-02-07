d96c65af039451e5ede31df49f447c4c0a04f486
Gabriel Ford <gabe@fordltc.net>
2025-02-07 12:31:39 -0500

    Added a chunk test for setting var.

--- clox/test/src/chunk_test.c
+++ clox/test/src/chunk_test.c
@@ -1,9 +1,10 @@
 
 #include "chunk.h"
+#include "compiler.h"
 #include "debug.h"
+#include "value.h"
 
 #include "helper_test.h"
-#include "value.h"
 
 const char TEST_WRITE_CHUNK_EXPECT[] = "== test chunk ==\n"
                                        "0000  123 OP_CONSTANT         0 '1.2'\n"
@@ -18,9 +19,25 @@ void       testWriteChunk() {
   disassembleChunk(&chunk, "test chunk");
   freeChunk(&chunk);
 }
+const char TEST_WRITE_CHUNK_FROM_SRC_EXPECT[] =
+    "== test chunk ==\n"
+    "0000    1 OP_CONSTANT         1 'greasy'\n"
+    "0002    | OP_DEFINE_GLOBAL    0 'beans'\n"
+    "0004    | OP_RETURN\n";
+void testWriteChunkFromSrc() {
+  Chunk chunk;
+  initChunk(&chunk);
+
+  compile("var beans = \"greasy\";", &chunk);
+
+  disassembleChunk(&chunk, "test chunk");
+  freeChunk(&chunk);
+}
 
 const TestCase CHUNK_TESTS[] = {
-    {"testWriteChunk", testWriteChunk, TEST_WRITE_CHUNK_EXPECT},
+    {"testWriteChunk",        testWriteChunk,        TEST_WRITE_CHUNK_EXPECT},
+    {"testWriteChunkFromSrc", testWriteChunkFromSrc,
+     TEST_WRITE_CHUNK_FROM_SRC_EXPECT                                       },
 };
 
 const int CHUNK_TESTS_COUNT = sizeof(CHUNK_TESTS) / sizeof(TestCase);
