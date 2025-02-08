2dfcf70ec4ee2feb40e3a789b66b80481896c489
Gabriel Ford <gabe@fordltc.net>
2025-02-08 15:21:46 -0500

    Added chunk test for locals.

--- clox/test/src/chunk_test.c
+++ clox/test/src/chunk_test.c
@@ -76,11 +76,41 @@ void testChunkGetGlobal() {
   disassembleChunk(&chunk, "test chunk");
   freeChunk(&chunk);
 }
+
+const char TEST_CHUNK_LOCAL_EXPECT[] =
+    "== test chunk ==\n"
+    "0000    2 OP_CONSTANT         0 'greasy'\n"
+    "0002    4 OP_CONSTANT         1 'humungooseeee'\n"
+    "0004    | OP_SET_LOCAL        0\n"
+    "0006    | OP_POP\n"
+    "0007    6 OP_GET_LOCAL        0\n"
+    "0009    | OP_PRINT\n"
+    "0010    7 OP_POP\n"
+    "0011    8 OP_RETURN\n";
+
+void testChunkLocal() {
+  Chunk chunk;
+  initChunk(&chunk);
+
+  compile("{\n"
+          "  var beans = \"greasy\";\n"
+          "  {\n"
+          "    beans = \"humungooseeee\";\n"
+          "  }\n"
+          "  print(beans);\n"
+          "}\n",
+          &chunk);
+
+  disassembleChunk(&chunk, "test chunk");
+  freeChunk(&chunk);
+}
+
 const TestCase CHUNK_TESTS[] = {
     {"testWriteChunk",     testWriteChunk,     TEST_WRITE_CHUNK_EXPECT     },
     {"testChunkDefGlobal", testChunkDefGlobal, TEST_CHUNK_DEF_GLOBAL_EXPECT},
     {"testChunkSetGlobal", testChunkSetGlobal, TEST_CHUNK_SET_GLOBAL_EXPECT},
     {"testChunkGetGlobal", testChunkGetGlobal, TEST_CHUNK_GET_GLOBAL_EXPECT},
+    {"testChunkLocal",     testChunkLocal,     TEST_CHUNK_LOCAL_EXPECT     },
 };
 
 const int CHUNK_TESTS_COUNT = sizeof(CHUNK_TESTS) / sizeof(TestCase);
