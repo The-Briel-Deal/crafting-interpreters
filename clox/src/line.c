bd181d895c266e42aaf1dc0a1b0d2571c90ea0c9
Gabriel Ford <gabe@fordltc.net>
2025-02-14 21:55:43 -0500

    Simplified insert line.

--- clox/src/line.c
+++ clox/src/line.c
@@ -18,11 +18,12 @@ void insertLine(struct Line *line, char c) {
     line->start    = realloc(line->start, line->capacity * sizeof(struct Line));
   }
   // Move all characters after index forward.
-  for (int i = line->length - 1; i >= line->pos; i--) {
-    line->start[i + 1] = line->start[i];
+  for (int i = line->length; i > line->pos; i--) {
+    line->start[i] = line->start[i-1];
   }
-  line->length++;
   line->start[line->pos] = c;
+
+  line->length++;
   line->pos++;
 }
 
