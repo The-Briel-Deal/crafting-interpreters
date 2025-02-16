1332309e45d6edba15e144f2ca6bc3288c1be06e
Gabriel Ford <gabe@fordltc.net>
2025-02-16 13:00:58 -0500

    Add switch case support to scanner.

--- clox/src/scanner.c
+++ clox/src/scanner.c
@@ -124,7 +124,14 @@ static TokenType identifierType() {
     case 'o': return checkKeyword(1, 1, "r", TOKEN_OR);
     case 'p': return checkKeyword(1, 4, "rint", TOKEN_PRINT);
     case 'r': return checkKeyword(1, 5, "eturn", TOKEN_RETURN);
-    case 's': return checkKeyword(1, 4, "uper", TOKEN_SUPER);
+    case 's':
+      if (scanner.current - scanner.start > 1) {
+        switch (scanner.start[1]) {
+          case 'u': return checkKeyword(2, 3, "per", TOKEN_SUPER);
+          case 'w': return checkKeyword(2, 4, "itch", TOKEN_SWITCH);
+        }
+      }
+
     case 't':
       if (scanner.current - scanner.start > 1) {
         switch (scanner.start[1]) {
