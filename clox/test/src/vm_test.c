ab9a4b33e0f4a3010047fd6a80152927a129c472
Gabriel Ford <gabe@fordltc.net>
2025-02-16 11:25:02 -0500

    Add test for while loop.

--- clox/test/src/vm_test.c
+++ clox/test/src/vm_test.c
@@ -114,12 +114,36 @@ const char TEST_BRANCH_2_EXPECT[] = "stmt is true\n";
 void testBranch2() {
   initVM();
 
-	// Evaluates like ((true and true) or (false and true))
+  // Evaluates like ((true and true) or (false and true))
   char source[] =
       "if (true and true or false and true)   \n"
       "  print(\"stmt is true\");             \n"
       "else                                   \n"
       "  print(\"stmt is false\");            \n";
+
+  interpret(source);
+}
+
+const char TEST_WHILE_1_EXPECT[] =
+    "1\n"
+    "2\n"
+    "3\n"
+    "4\n"
+    "5\n"
+    "6\n"
+    "7\n"
+    "8\n"
+    "9\n";
+void testWhile1() {
+  initVM();
+
+  // Evaluates like ((true and true) or (false and true))
+  char source[] =
+      "var num = 1;                \n"
+      "while (num < 10) {          \n"
+      "  print(num); \n"
+      "  num = num + 1;            \n"
+      "}                           \n";
 
   interpret(source);
 }
@@ -137,6 +161,7 @@ const TestCase VM_TESTS[] = {
     {"testLocal2",        testLocal2,        TEST_LOCAL_2_EXPECT        },
     {"testBranch1",       testBranch1,       TEST_BRANCH_1_EXPECT       },
     {"testBranch2",       testBranch2,       TEST_BRANCH_2_EXPECT       },
+    {"testWhile1",        testWhile1,        TEST_WHILE_1_EXPECT        },
 };
 
 const int VM_TESTS_COUNT = sizeof(VM_TESTS) / sizeof(TestCase);
