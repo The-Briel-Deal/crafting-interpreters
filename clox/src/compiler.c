f1ce3eadf46405daf8771291e3697da955b97532
Gabriel Ford <gabe@fordltc.net>
2025-02-16 10:08:36 -0500

    Define and compiler handler.

--- clox/src/compiler.c
+++ clox/src/compiler.c
@@ -270,8 +270,16 @@ static void defineVariable(uint8_t global) {
     markInitialized();
     return;
   }
-
   emitBytes(OP_DEFINE_GLOBAL, global);
+}
+
+static void and_(bool canAssign) {
+  int endJump = emitJump(OP_JUMP_IF_FALSE);
+
+  emitByte(OP_POP);
+  parsePrecedence(PREC_AND);
+
+  patchJump(endJump);
 }
 
 static void binary(bool canAssign) {
@@ -381,7 +389,7 @@ ParseRule rules[] = {
     [TOKEN_IDENTIFIER]    = {variable, NULL,   PREC_NONE      },
     [TOKEN_STRING]        = {string,   NULL,   PREC_NONE      },
     [TOKEN_NUMBER]        = {number,   NULL,   PREC_NONE      },
-    [TOKEN_AND]           = {NULL,     NULL,   PREC_NONE      },
+    [TOKEN_AND]           = {NULL,     and_,   PREC_AND       },
     [TOKEN_CLASS]         = {NULL,     NULL,   PREC_NONE      },
     [TOKEN_ELSE]          = {NULL,     NULL,   PREC_NONE      },
     [TOKEN_FALSE]         = {literal,  NULL,   PREC_NONE      },
