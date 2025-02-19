b7e66c769b31d0b16f5e54311bbc109a4bc2a07f
Gabriel Ford <gabe@fordltc.net>
2025-02-18 23:25:56 -0500

    Finish Chap 24.4.2 - A stack of compilers.

--- clox/src/compiler.c
+++ clox/src/compiler.c
@@ -9,6 +9,7 @@
 #include "compiler.h"
 #include "object.h"
 #include "scanner.h"
+#include "value.h"
 
 #ifdef DEBUG_PRINT_CODE
 #include "debug.h"
@@ -50,7 +51,8 @@ typedef struct {
 
 typedef enum { TYPE_FUNCTION, TYPE_SCRIPT } FunctionType;
 
-typedef struct {
+typedef struct Compiler {
+  struct Compiler *enclosing;
   ObjFunction *function;
   FunctionType type;
 
@@ -180,6 +182,7 @@ static void patchJump(int offset) {
 }
 
 static void initCompiler(Compiler *compiler, FunctionType type) {
+  compiler->enclosing  = current;
   compiler->function   = NULL;
   compiler->type       = type;
   compiler->localCount = 0;
@@ -204,6 +207,8 @@ static ObjFunction *endCompiler() {
                                          : "<script>");
   }
 #endif
+
+  current = current->enclosing;
   return function;
 }
 
@@ -290,6 +295,8 @@ static uint8_t parseVariable(const char *errorMessage) {
 }
 
 static void markInitialized() {
+  if (current->scopeDepth == 0)
+    return;
   current->locals[current->localCount - 1].depth = current->scopeDepth;
 }
 
@@ -486,6 +493,27 @@ static void block() {
   consume(TOKEN_RIGHT_BRACE, "Expect '}' after block.");
 }
 
+static void function(FunctionType type) {
+  Compiler compiler;
+  initCompiler(&compiler, type);
+  beginScope();
+
+  consume(TOKEN_LEFT_PAREN, "Expect '(' after function name.");
+  consume(TOKEN_RIGHT_PAREN, "Expect ')' after parameters.");
+  consume(TOKEN_LEFT_BRACE, "Expect '{' before function body.");
+  block();
+
+  ObjFunction *function = endCompiler();
+  emitBytes(OP_CONSTANT, makeConstant(OBJ_VAL(function)));
+}
+
+static void funDeclaration() {
+  uint8_t global = parseVariable("Expect function name.");
+  markInitialized();
+  function(TYPE_FUNCTION);
+  defineVariable(global);
+}
+
 static void expressionStmt() {
   expression();
   consume(TOKEN_SEMICOLON, "Expect ';' after expression.");
@@ -614,7 +642,9 @@ static void varDeclaration() {
 }
 
 static void declaration() {
-  if (match(TOKEN_VAR)) {
+  if (match(TOKEN_FUN)) {
+    funDeclaration();
+  } else if (match(TOKEN_VAR)) {
     varDeclaration();
   } else {
     statement();
