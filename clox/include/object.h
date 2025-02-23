bb6d3374aa2967877299b69a6c154cb459c7c47c
Gabriel Ford <gabe@fordltc.net>
2025-02-23 13:56:46 -0500

    Finish Chap 25.3 - Upvalue Objects.

--- clox/include/object.h
+++ clox/include/object.h
@@ -24,6 +24,7 @@ typedef enum {
   OBJ_FUNCTION,
   OBJ_NATIVE,
   OBJ_STRING,
+  OBJ_UPVALUE
 } ObjType;
 
 struct Obj {
@@ -56,15 +57,23 @@ struct ObjString {
   uint32_t hash;
 };
 
+typedef struct ObjUpvalue {
+  Obj obj;
+  Value *location;
+} ObjUpvalue;
+
 typedef struct {
   Obj obj;
   ObjFunction *function;
+  ObjUpvalue **upvalues;
+  int upvalueCount;
 } ObjClosure;
 
 ObjClosure *newClosure(ObjFunction *function);
 
 ObjString *takeString(char *chars, int length);
 ObjString *copyString(const char *chars, int length);
+ObjUpvalue *newUpvalue(Value *slot);
 void printObject(Value value);
 
 static inline bool isObjType(Value value, ObjType type) {
--- clox/src/memory.c
+++ clox/src/memory.c
@@ -37,9 +37,12 @@ static void freeObject(Obj *object) {
       break;
     }
     case OBJ_CLOSURE: {
+      ObjClosure *closure = (ObjClosure *)object;
+      FREE_ARRAY(ObjUpvalue, closure->upvalues, closure->upvalueCount);
       FREE(ObjClosure, object);
       break;
     }
+    case OBJ_UPVALUE: FREE(ObjUpvalue, object); break;
   }
 }
 
