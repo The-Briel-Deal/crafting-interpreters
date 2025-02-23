80c0a3e5c77bcc906e244e2068c6be725e690080
Gabriel Ford <gabe@fordltc.net>
2025-02-22 22:47:33 -0500

    Finish Chap 25.2 - Upvalues

--- clox/src/debug.c
+++ clox/src/debug.c
@@ -3,6 +3,7 @@
 
 #include "chunk.h"
 #include "debug.h"
+#include "object.h"
 #include "value.h"
 
 void disassembleChunk(Chunk *chunk, const char *name) {
@@ -72,17 +73,21 @@ int disassembleInstruction(Chunk *chunk, int offset) {
       return constantInstruction("OP_DEFINE_GLOBAL", chunk, offset);
     case OP_SET_GLOBAL:
       return constantInstruction("OP_SET_GLOBAL", chunk, offset);
-    case OP_EQUAL   : return simpleInstruction("OP_EQUAL", offset);
-    case OP_GREATER : return simpleInstruction("OP_GREATER", offset);
-    case OP_LESS    : return simpleInstruction("OP_LESS", offset);
-    case OP_ADD     : return simpleInstruction("OP_ADD", offset);
-    case OP_SUBTRACT: return simpleInstruction("OP_SUBTRACT", offset);
-    case OP_MULTIPLY: return simpleInstruction("OP_MULTIPLY", offset);
-    case OP_DIVIDE  : return simpleInstruction("OP_DIVIDE", offset);
-    case OP_NOT     : return simpleInstruction("OP_NOT", offset);
-    case OP_NEGATE  : return simpleInstruction("OP_NEGATE", offset);
-    case OP_PRINT   : return simpleInstruction("OP_PRINT", offset);
-    case OP_JUMP    : return jumpInstruction("OP_JUMP", 1, chunk, offset);
+    case OP_GET_UPVALUE:
+      return byteInstruction("OP_GET_UPVALUE", chunk, offset);
+    case OP_SET_UPVALUE:
+      return byteInstruction("OP_SET_UPVALUE", chunk, offset);
+    case OP_EQUAL      : return simpleInstruction("OP_EQUAL", offset);
+    case OP_GREATER    : return simpleInstruction("OP_GREATER", offset);
+    case OP_LESS       : return simpleInstruction("OP_LESS", offset);
+    case OP_ADD        : return simpleInstruction("OP_ADD", offset);
+    case OP_SUBTRACT   : return simpleInstruction("OP_SUBTRACT", offset);
+    case OP_MULTIPLY   : return simpleInstruction("OP_MULTIPLY", offset);
+    case OP_DIVIDE     : return simpleInstruction("OP_DIVIDE", offset);
+    case OP_NOT        : return simpleInstruction("OP_NOT", offset);
+    case OP_NEGATE     : return simpleInstruction("OP_NEGATE", offset);
+    case OP_PRINT      : return simpleInstruction("OP_PRINT", offset);
+    case OP_JUMP       : return jumpInstruction("OP_JUMP", 1, chunk, offset);
     case OP_JUMP_IF_FALSE:
       return jumpInstruction("OP_JUMP_IF_FALSE", 1, chunk, offset);
     case OP_LOOP   : return jumpInstruction("OP_LOOP", -1, chunk, offset);
@@ -93,6 +98,15 @@ int disassembleInstruction(Chunk *chunk, int offset) {
       printf("%-16s %4d ", "OP_CLOSURE", constant);
       printValue(chunk->constants.values[constant]);
       printf("\r\n");
+
+      ObjFunction *function = AS_FUNCTION(chunk->constants.values[constant]);
+      for (int j = 0; j < function->upvalueCount; j++) {
+        int isLocal = chunk->code[offset++];
+        int index   = chunk->code[offset++];
+        printf("%04d      |                     %s %d\n", offset - 2,
+               isLocal ? "local" : "upvalue", index);
+      }
+
       return offset;
     }
     case OP_RETURN: return simpleInstruction("OP_RETURN", offset);
