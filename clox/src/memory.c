f8185c090aa59057b6b6a814bebd6cdbaf936e87
Gabriel Ford <gabe@fordltc.net>
2025-02-28 20:13:47 -0500

    Finish Chap 26.4 - Graying Objects.

--- clox/src/memory.c
+++ clox/src/memory.c
@@ -65,6 +65,8 @@ static void freeObject(Obj *object) {
 void markObject(Obj *object) {
   if (object == NULL)
     return;
+  if (object->isMarked)
+    return;
 #ifdef DEBUG_LOG_GC
   printf("%p mark ", (void *)object);
   printValue(OBJ_VAL(object));
@@ -90,6 +92,39 @@ void markValue(Value value) {
     markObject(AS_OBJ(value));
 }
 
+static void markArray(ValueArray *array) {
+  for (int i = 0; i < array->count; i++) {
+    markValue(array->values[i]);
+  }
+}
+
+static void blackenObject(Obj *object) {
+#ifdef DEBUG_LOG_GC
+  printf("%p blacken ", (void *)object);
+  printValue(OBJ_VAL(object));
+  printf("\n");
+#endif
+  switch (object->type) {
+    case OBJ_CLOSURE: {
+      ObjClosure *closure = (ObjClosure *)object;
+      markObject((Obj *)closure->function);
+      for (int i = 0; i < closure->upvalueCount; i++) {
+        markObject((Obj *)closure->upvalues[i]);
+      }
+      break;
+    }
+    case OBJ_FUNCTION: {
+      ObjFunction *function = (ObjFunction *)object;
+      markObject((Obj *)function->name);
+      markArray(&function->chunk.constants);
+      break;
+    }
+    case OBJ_UPVALUE: markValue(((ObjUpvalue *)object)->closed); break;
+    case OBJ_NATIVE :
+    case OBJ_STRING : break;
+  }
+}
+
 static void markRoots() {
   for (Value *slot = vm.stack; slot < vm.stackTop; slot++) {
     markValue(*slot);
@@ -108,12 +143,20 @@ static void markRoots() {
   markCompilerRoots();
 }
 
+static void traceReferences() {
+  while (vm.grayCount > 0) {
+    Obj *object = vm.grayStack[--vm.grayCount];
+    blackenObject(object);
+  }
+}
+
 void collectGarbage() {
 #ifdef DEBUG_LOG_GC
   printf("-- gc begin\n");
 #endif
 
   markRoots();
+  traceReferences();
 
 #ifdef DEBUG_LOG_GC
   printf("-- gc end\n");
