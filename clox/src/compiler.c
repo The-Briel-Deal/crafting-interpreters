41a8a4702118e6575e156ba29204408943e2c768
Gabriel Ford <gabe@fordltc.net>
2025-03-09 19:13:01 -0400

    Finish Chap 28.3.0 - This.

--- clox/src/compiler.c
+++ clox/src/compiler.c
@@ -56,7 +56,7 @@ typedef struct {
   bool isLocal;
 } Upvalue;
 
-typedef enum { TYPE_FUNCTION, TYPE_SCRIPT } FunctionType;
+typedef enum { TYPE_FUNCTION, TYPE_METHOD, TYPE_SCRIPT } FunctionType;
 
 typedef struct Compiler {
   struct Compiler *enclosing;
@@ -203,11 +203,16 @@ static void initCompiler(Compiler *compiler, FunctionType type) {
         copyString(parser.previous.start, parser.previous.length);
   }
 
-  Local *local       = &current->locals[current->localCount++];
-  local->depth       = 0;
-  local->isCaptured  = false;
-  local->name.start  = "";
-  local->name.length = 0;
+  Local *local      = &current->locals[current->localCount++];
+  local->depth      = 0;
+  local->isCaptured = false;
+  if (type != TYPE_FUNCTION) {
+    local->name.start  = "this";
+    local->name.length = 4;
+  } else {
+    local->name.start  = "";
+    local->name.length = 0;
+  }
 }
 
 static ObjFunction *endCompiler() {
@@ -490,6 +495,10 @@ static void variable(bool canAssign) {
   namedVariable(parser.previous, canAssign);
 }
 
+static void this_(bool canAssign) {
+  variable(false);
+}
+
 static void unary(bool canAssign) {
   TokenType operatorType = parser.previous.type;
 
@@ -537,7 +546,7 @@ ParseRule rules[] = {
     [TOKEN_PRINT]         = {NULL,     NULL,   PREC_NONE      },
     [TOKEN_RETURN]        = {NULL,     NULL,   PREC_NONE      },
     [TOKEN_SUPER]         = {NULL,     NULL,   PREC_NONE      },
-    [TOKEN_THIS]          = {NULL,     NULL,   PREC_NONE      },
+    [TOKEN_THIS]          = {this_,    NULL,   PREC_NONE      },
     [TOKEN_TRUE]          = {literal,  NULL,   PREC_NONE      },
     [TOKEN_VAR]           = {NULL,     NULL,   PREC_NONE      },
     [TOKEN_WHILE]         = {NULL,     NULL,   PREC_NONE      },
@@ -615,27 +624,28 @@ static void function(FunctionType type) {
 static void method() {
   consume(TOKEN_IDENTIFIER, "Expect method name.");
   uint8_t constant = identifierConstant(&parser.previous);
-	FunctionType type = TYPE_FUNCTION;
-	function(type);
+
+  FunctionType type = TYPE_METHOD;
+  function(type);
   emitBytes(OP_METHOD, constant);
 }
 
 static void classDeclaration() {
   consume(TOKEN_IDENTIFIER, "Expect class name.");
-	Token className = parser.previous;
+  Token className      = parser.previous;
   uint8_t nameConstant = identifierConstant(&parser.previous);
   declareVariable();
 
   emitBytes(OP_CLASS, nameConstant);
   defineVariable(nameConstant);
 
-	namedVariable(className, false);
+  namedVariable(className, false);
   consume(TOKEN_LEFT_BRACE, "Expect '{' before class body.");
   while (!check(TOKEN_RIGHT_BRACE) && !check(TOKEN_EOF)) {
     method();
   }
   consume(TOKEN_RIGHT_BRACE, "Expect '}' after class body.");
-	emitByte(OP_POP);
+  emitByte(OP_POP);
 }
 
 static void funDeclaration() {
