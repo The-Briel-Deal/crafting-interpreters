964911b8af0dee2214c45d1dba267a105369a5c1
Gabriel Ford <gabe@fordltc.net>
2025-03-22 13:45:00 -0400

    Finish Chap 30.3.4 - NaN Boxing, Bools and Nil.

--- clox/include/value.h
+++ clox/include/value.h
@@ -10,12 +10,25 @@ typedef struct ObjString ObjString;
 
 #ifdef NAN_BOXING
 
-#define QNAN ((uint64_t)0x7ffc000000000000)
+#define QNAN      ((uint64_t)0x7ffc000000000000)
+
+#define TAG_NIL   1
+#define TAG_FALSE 2
+#define TAG_TRUE  3
 
 typedef uint64_t Value;
 
+#define IS_BOOL(value)   (((value) | 1) == TRUE_VAL)
+#define IS_NIL(value)    ((value) == NIL_VAL)
 #define IS_NUMBER(value) (((value) & QNAN) != QNAN)
+
+#define AS_BOOL(value)   ((value) == TRUE_VAL)
 #define AS_NUMBER(value) valueToNum(value)
+
+#define BOOL_VAL(b)      ((b) ? TRUE_VAL : FALSE_VAL)
+#define FALSE_VAL        ((Value)(uint64_t)(QNAN | TAG_FALSE))
+#define TRUE_VAL         ((Value)(uint64_t)(QNAN | TAG_TRUE))
+#define NIL_VAL          ((Value)(uint64_t)(QNAN | TAG_NIL))
 #define NUMBER_VAL(num)  numToValue(num)
 
 // These should optimize the memcpy away.
