f636437b47d92d2a48cc4f7dc616fd858ff9b12a
Gabriel Ford <gabe@fordltc.net>
2025-03-23 12:11:45 -0400

    Handle printValue() small str case.

--- clox/src/value.c
+++ clox/src/value.c
@@ -42,10 +42,11 @@ void printValue(Value value) {
   }
 #else
   switch (value.type) {
-    case VAL_BOOL  : printf(AS_BOOL(value) ? "true" : "false"); break;
-    case VAL_NIL   : printf("nil"); break;
-    case VAL_NUMBER: printf("%g", AS_NUMBER(value)); break;
-    case VAL_OBJ   : printObject(value); break;
+    case VAL_BOOL     : printf(AS_BOOL(value) ? "true" : "false"); break;
+    case VAL_NIL      : printf("nil"); break;
+    case VAL_NUMBER   : printf("%g", AS_NUMBER(value)); break;
+    case VAL_OBJ      : printObject(value); break;
+    case VAL_SMALL_STR: printf("%s", AS_SMALL_STR(value));
   }
 #endif
 }
