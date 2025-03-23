e0eed4e94c2c86523397347ff9c8073a653f85c6
Gabriel Ford <gabe@fordltc.net>
2025-03-23 11:52:51 -0400

    Add tagged union option for small strs under 7 chars.

--- clox/include/value.h
+++ clox/include/value.h
@@ -1,6 +1,7 @@
 #ifndef clox_value_h
 #define clox_value_h
 
+#include <assert.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <string.h>
@@ -49,7 +50,13 @@ static inline Value numToValue(double num) {
 
 #else
 
-typedef enum { VAL_BOOL, VAL_NIL, VAL_NUMBER, VAL_OBJ } ValueType;
+typedef enum {
+  VAL_BOOL,
+  VAL_NIL,
+  VAL_NUMBER,
+  VAL_OBJ,
+  VAL_SMALL_STR
+} ValueType;
 
 typedef struct {
   ValueType type;
@@ -57,22 +64,35 @@ typedef struct {
     bool boolean;
     double number;
     Obj *obj;
+    char smallStr[8];
   } as;
 } Value;
 
-#define IS_BOOL(value)    ((value).type == VAL_BOOL)
-#define IS_NIL(value)     ((value).type == VAL_NIL)
-#define IS_NUMBER(value)  ((value).type == VAL_NUMBER)
-#define IS_OBJ(value)     ((value).type == VAL_OBJ)
+#define IS_BOOL(value)      ((value).type == VAL_BOOL)
+#define IS_NIL(value)       ((value).type == VAL_NIL)
+#define IS_NUMBER(value)    ((value).type == VAL_NUMBER)
+#define IS_SMALL_STR(value) ((value).type == VAL_SMALL_STR)
+#define IS_OBJ(value)       ((value).type == VAL_OBJ)
 
-#define AS_OBJ(value)     ((value).as.obj)
-#define AS_BOOL(value)    ((value).as.boolean)
-#define AS_NUMBER(value)  ((value).as.number)
+#define AS_OBJ(value)       ((value).as.obj)
+#define AS_BOOL(value)      ((value).as.boolean)
+#define AS_NUMBER(value)    ((value).as.number)
+#define AS_SMALL_STR(value) ((value).as.smallStr)
 
-#define BOOL_VAL(value)   ((Value){VAL_BOOL, {.boolean = value}})
-#define NIL_VAL           ((Value){VAL_NIL, {.number = 0}})
-#define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = value}})
-#define OBJ_VAL(object)   ((Value){VAL_OBJ, {.obj = (Obj *)object}})
+#define BOOL_VAL(value)     ((Value){VAL_BOOL, {.boolean = value}})
+#define NIL_VAL             ((Value){VAL_NIL, {.number = 0}})
+#define NUMBER_VAL(value)   ((Value){VAL_NUMBER, {.number = value}})
+#define SMALL_STR_VAL(str)  (newSmallStr(str))
+#define OBJ_VAL(object)     ((Value){VAL_OBJ, {.obj = (Obj *)object}})
+
+/* Should never be passed a string longer than 7 chars, the 8th is reserved for
+ * null terminator.*/
+inline Value newSmallStr(const char *str) {
+  Value value = {VAL_SMALL_STR};
+  strncpy(value.as.smallStr, str, sizeof(value.as.smallStr));
+  assert(value.as.smallStr[sizeof(value.as.smallStr) - 1] == '\0');
+  return value;
+}
 
 #endif
 
