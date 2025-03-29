ff58516e249247b1cca2efab3766b2f6c0ce2e25
Gabriel Ford <gabe@fordltc.net>
2025-03-29 11:08:31 -0400

    Create function to make small strings.

--- clox/include/value.h
+++ clox/include/value.h
@@ -1,6 +1,7 @@
 #ifndef clox_value_h
 #define clox_value_h
 
+#include <assert.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <string.h>
@@ -62,6 +63,13 @@ typedef struct {
   char start[7];
 } SmallStr;
 
+inline SmallStr newSmallStr(uint8_t len, char* start) {
+	assert(len <= 7);
+	SmallStr smallStr = {.len = len};
+	strncpy(smallStr.start, start, len);
+	return smallStr;
+}
+
 typedef struct {
   ValueType type;
   union {
@@ -72,19 +80,20 @@ typedef struct {
   } as;
 } Value;
 
-#define IS_BOOL(value)    ((value).type == VAL_BOOL)
-#define IS_NIL(value)     ((value).type == VAL_NIL)
-#define IS_NUMBER(value)  ((value).type == VAL_NUMBER)
-#define IS_OBJ(value)     ((value).type == VAL_OBJ)
+#define IS_BOOL(value)          ((value).type == VAL_BOOL)
+#define IS_NIL(value)           ((value).type == VAL_NIL)
+#define IS_NUMBER(value)        ((value).type == VAL_NUMBER)
+#define IS_OBJ(value)           ((value).type == VAL_OBJ)
 
-#define AS_OBJ(value)     ((value).as.obj)
-#define AS_BOOL(value)    ((value).as.boolean)
-#define AS_NUMBER(value)  ((value).as.number)
+#define AS_OBJ(value)           ((value).as.obj)
+#define AS_BOOL(value)          ((value).as.boolean)
+#define AS_NUMBER(value)        ((value).as.number)
 
-#define BOOL_VAL(value)   ((Value){VAL_BOOL, {.boolean = value}})
-#define NIL_VAL           ((Value){VAL_NIL, {.number = 0}})
-#define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = value}})
-#define OBJ_VAL(object)   ((Value){VAL_OBJ, {.obj = (Obj *)object}})
+#define BOOL_VAL(value)         ((Value){VAL_BOOL, {.boolean = value}})
+#define NIL_VAL                 ((Value){VAL_NIL, {.number = 0}})
+#define NUMBER_VAL(value)       ((Value){VAL_NUMBER, {.number = value}})
+#define OBJ_VAL(object)         ((Value){VAL_OBJ, {.obj = (Obj *)object}})
+#define SMALL_STR_VAL(smallStr) ((Value){VAL_SMALL_STR, {.smallStr = smallStr}})
 
 #endif
 
