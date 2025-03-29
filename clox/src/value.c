83bd6faa87e8371dd5aba15455821c363369e5c5
Gabriel Ford <gabe@fordltc.net>
2025-03-29 13:57:00 -0400

    Add return statements for when NDEBUG is on.

--- clox/src/value.c
+++ clox/src/value.c
@@ -82,19 +82,19 @@ Value smallStrToObjString(Value smallStrVal) {
 char *getStringChars(Value *value) {
   if (IS_STRING(*value)) {
     return AS_STRING(*value)->chars;
-
   } else if (IS_SMALL_STR(*value)) {
     return AS_SMALL_STR(*value).start;
   }
   assert(false);
+  return NULL;
 }
 
 int getStringLen(Value *value) {
   if (IS_STRING(*value)) {
     return AS_STRING(*value)->length;
-
   } else if (IS_SMALL_STR(*value)) {
     return AS_SMALL_STR(*value).len;
   }
   assert(false);
+  return -1;
 }
